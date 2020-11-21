20cb093c1530aa9fa8d157be0d44d6195d11f2f5
hadasjk <hadasjk33@gmail.com>
2020-11-21 17:04:46 +0900

    Only Move

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float jump;
+    public float moveSpeed;
+
+    private void Update()
+    {
+
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+            GetComponent<Rigidbody2D>().velocity = new Vector3(/*속력벡터*/ GetComponent<Rigidbody2D>().velocity.x, jump);
+
+            }
+        
+    }
+
+    private void Move()
+    {
+        Vector3 moveVelocity = Vector3.right;
+
+        transform.position += moveVelocity * moveSpeed * Time.deltaTime;
+    }
+
+   
+}
