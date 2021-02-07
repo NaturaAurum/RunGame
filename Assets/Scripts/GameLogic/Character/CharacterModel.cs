796cf15656eb7aed8ed180e1e61c94740e0c0ee6
NaturaAurum <taewoo.kim@5minlab.com>
2021-02-07 22:39:06 +0900

    주석 추가

--- Assets/Scripts/GameLogic/Character/CharacterModel.cs
+++ Assets/Scripts/GameLogic/Character/CharacterModel.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Stella.GameLogic.Character
@@ -8,5 +9,10 @@ namespace Stella.GameLogic.Character
     public class CharacterModel : MonoBehaviour
     {
         private CharacterBase _characterBase = null;
+
+        private void Awake()
+        {
+            // TODO : 여기서 CharacterData를 불러와서 모델 프리팹을 로드하는 것을 만들고 싶다.
+        }
     }
 }
