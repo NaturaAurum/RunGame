86d794989332640971aa1b562279bbc0d177fc14
NaturaAurum <taewoo.kim@5minlab.com>
2021-03-07 03:01:15 +0900

    Environment rename, map editor 제작 시작

---
+++ Assets/Scripts/GameLogic/Environment/Map/MapEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stella.GameLogic.Environment.Map
+{
+    public class MapEditor : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
