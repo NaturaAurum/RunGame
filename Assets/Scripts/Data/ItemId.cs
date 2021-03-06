7d7e850db2acdf9aed2730b8b3fdbf4155a66f81
NaturaAurum <taewoo.kim@5minlab.com>
2021-03-07 07:58:42 +0900

    Data 클래스 Base 클래스

---
+++ Assets/Scripts/Data/ItemId.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Stella.Data.Enums;
+
+namespace Stella.Data
+{
+    [Serializable]
+    public struct ItemId
+    {
+        public ItemType Type;
+        public MapId MapId;
+        public int Id;
+
+        public static ItemId ByType(
+            ItemType type
+        )
+        {
+            return By(type, MapId.None, 0);
+        }
+
+        public static ItemId ByMapId(
+            MapId mapId
+        )
+        {
+            return By(ItemType.None, mapId, 0);
+        }
+        
+        public static ItemId By(
+            ItemType itemType,
+            MapId mapId,
+            int id
+        )
+        {
+            var itemId = new ItemId();
+            itemId.Type = itemType;
+            itemId.MapId = mapId;
+            itemId.Id = id;
+            return itemId;
+        }
+
+        public static bool operator ==(ItemId i1, ItemId i2)
+        {
+            var typeSame = i1.Type == i2.Type;
+            var mapSame = i1.MapId == i2.MapId;
+            var idSame = i1.Id == i2.Id;
+            return typeSame && mapSame && idSame;
+        }
+
+        public static bool operator !=(ItemId i1, ItemId i2) => !(i1 == i2);
+        
+    }
+
+    [Serializable]
+    public struct MapId
+    {
+        public static MapId None => ByType(MapType.None);
+        
+        public MapThemeType Theme;
+        public MapType Type;
+
+        public static MapId ByType(
+            MapType type
+        )
+        {
+            return By(MapThemeType.None, type);
+        }
+        
+        public static MapId ByTheme(
+            MapThemeType theme
+        )
+        {
+            return By(theme, MapType.None);
+        }
+        
+        public static MapId By(
+            MapThemeType theme,
+            MapType map
+        )
+        {
+            var mapId = new MapId();
+            mapId.Theme = theme;
+            mapId.Type = map;
+
+            return mapId;
+        }
+
+        public static bool operator ==(MapId x1, MapId x2)
+        {
+            var themeSame = x1.Theme == x2.Theme;
+            var typeSame = x1.Type == x2.Type;
+            return themeSame && typeSame;
+        }
+
+        public static bool operator !=(MapId x1, MapId x2) => !(x1 == x2);
+    }
+}
---
+++ Assets/Scripts/Data/KeyTable.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Stella.Data
+{
+    public interface IKey<T>
+    {
+        T Key { get; }
+    }
+
+    public interface IKeyTable<TKey, TValue> 
+        where TValue : IKey<TKey>
+    {
+        IReadOnlyList<TValue> Values { get; }
+        
+        IEnumerable<TKey> Keys { get; }
+
+        // void SetValues(IEnumerable<TValue> values);
+    }
+    
+    public class KeyTable<TKey, TValue> : ScriptableObject, IKeyTable<TKey, TValue> 
+        where TValue : IKey<TKey>
+    {
+        public int Count => values.Count;
+        
+        public IReadOnlyList<TValue> Values => values;
+        public IEnumerable<TKey> Keys => values.Select(v => v.Key);
+        
+        [TableList(AlwaysExpanded = true)]
+        [SerializeField]
+        protected List<TValue> values = null;
+
+        public void Add(TValue value)
+        {
+            values.Add(value);
+        }
+
+        public bool Contains(TKey key) => Keys.Contains(key);
+        public bool Contains(TValue value) => values.Contains(value);
+
+        public TValue GetValue(TKey key)
+        {
+            foreach (var value in values)
+            {
+                if (value.Key.Equals(key))
+                {
+                    return value;
+                }
+            }
+
+            return default;
+        }
+
+        public void Remove(TKey key)
+        {
+            for (var i = values.Count - 1; i >= 0; i--)
+            {
+                var value = values[i];
+                if (value.Key.Equals(key))
+                {
+                    values.Remove(value);
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            values.Clear();
+        }
+    }
+}
