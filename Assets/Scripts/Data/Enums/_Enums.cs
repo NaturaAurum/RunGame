684186ec09140bd9029c60986f4cad23d9e36323
NaturaAurum <taewoo.kim@5minlab.com>
2021-03-07 07:58:52 +0900

    Enum 모아두는 스크립트 생성

---
+++ Assets/Scripts/Data/Enums/_Enums.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Stella.Utils;
+
+namespace Stella.Data.Enums
+{
+    public enum ItemType
+    {
+        None,
+        Floor,
+        Water,
+        Obstacles,
+    }
+
+    public enum MapThemeType
+    {
+        None,
+        Common, // Dirt
+        Autumn,
+        Spring,
+        Winter,
+    }
+
+    public enum MapType
+    {
+        None,
+        Two, // 2d
+        Two_Five, // 2.5d
+    }
+
+    public enum CommonBlockId
+    {
+        Asset40,
+        Asset41,
+        Column,
+        ColumnDown,
+        ColumnUp,
+        Dirt,
+        DirtDown,
+        DirtLeft,
+        DirtLeftCorner,
+        DirtRight,
+        DirtRightCorner,
+    }
+
+    public enum TileBlockId
+    {
+        Grass,
+        GrassCliffLeft,
+        GrassCliffMid,
+        GrassCliffRight,
+        GrassColumn,
+        GrassHillLeft,
+        GrassHillLeft2DownShadow,
+        GrassHillLeft2,
+        GrassHillRight,
+        GrassHillRight2DownShadow,
+        GrassHillRight2,
+        GrassJoinHillLeft,
+        GrassJoinHillLeft2DownShadow,
+        GrassJoinHillLeft2,
+        GrassJoinHillLeftAndRight,
+        GrassJoinHillRight,
+        GrassJoinHillRight2DownShadow,
+        GrassJoinHillRight2AndLeft2DownShadow,
+        GrassJoinHillRight2AndLeft2,
+        GrassJoinHillRight2,
+        GrassLeft,
+        GrassMid,
+        GrassRight,
+    }
+    
+    public class TileBlockIdUtil : EnumUtil<TileBlockId> {}
+    public class CommonBlockIdUtil : EnumUtil<CommonBlockId> {}
+}
